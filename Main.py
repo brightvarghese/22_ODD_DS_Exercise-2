846784d58821d683107bdc5e0e500a77e538afda
brightvarghese <77091625+brightvarghese@users.noreply.github.com>
2022-07-21 13:11:36 +0530

    Initial commit of Coding template

---
+++ Main.py
@@ -0,0 +1,72 @@
+class Evaluate:
+    """This class vaildates and evaluate postfix expression.
+
+    Attributes:
+        top: An integer which denotes the index of the element at the top of the stack currently.
+        size_of_stack: An integer which represents the size of stack.
+        stack: A List which acts as a Stack.
+    """
+    # Write your code here
+    
+  def __init__(self, size):
+    """Inits Evaluate with top, size_of_stack and stack.
+    Arguments:
+      size: An integer which represents size of stack.
+    """
+    self.top = -1
+    self.size_of_stack = size
+    self.stack = []
+
+  def isEmpty(self):
+    """ 
+    Check whether the stack is empty.
+    Returns:
+      True if it is empty, else returns False.
+    """
+    # Write your code here
+
+  def pop(self):
+    """ 
+    Do pop operation if the stack is not empty.
+    Returns:
+      The data which is popped out if the stack is not empty.
+    """
+    # Write your code here
+
+  def push(self, operand):
+    """ 
+    Push the operand to stack if the stack is not full.
+    Arguments:
+      operand: The operand to be pushed.
+    """
+    # Write your code here
+
+  def validate_postfix_expression(self, expression):
+    """ 
+    Check whether the expression is a valid postfix expression.
+    Arguments:
+      expression: A String which represents the expression to be validated.
+    Returns:
+      True if the expression is valid, else returns False.
+    """
+    # Write your code here
+
+  def evaluate_postfix_expression(self, expression):
+    """ 
+    Evaluate the postfix expression 
+    Arguments:
+      expression: A String which represents the the expression to be evaluated
+    Returns:
+      The result of evaluated postfix expression.
+    """
+    # Write your code here
+
+
+# Do not change the following code
+postfix_expression = input() # Read postfix expression
+tokens = postfix_expression.split()
+evaluate = Evaluate(len(tokens))
+if evaluate.validate_postfix_expression(tokens):
+  print(evaluate.evaluate_postfix_expression(tokens))
+else:
+  print('Invalid postfix expression')
