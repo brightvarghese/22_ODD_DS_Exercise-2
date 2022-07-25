72f36e8a00b370a3735d56d82821535934b534d8
brightvarghese <77091625+brightvarghese@users.noreply.github.com>
2022-07-25 09:45:28 +0530

    Fixed intent

--- Main.py
+++ Main.py
@@ -1,13 +1,13 @@
 class Evaluate:
-    """This class vaildates and evaluate postfix expression.
-
-    Attributes:
-        top: An integer which denotes the index of the element at the top of the stack currently.
-        size_of_stack: An integer which represents the size of stack.
-        stack: A List which acts as a Stack.
-    """
+  """This class vaildates and evaluate postfix expression.
+  Attributes:
+      top: An integer which denotes the index of the element at the top of the stack currently.
+      size_of_stack: An integer which represents the size of stack.
+      stack: A List which acts as a Stack.
+  """
     # Write your code here
-    
+
+
   def __init__(self, size):
     """Inits Evaluate with top, size_of_stack and stack.
     Arguments:
@@ -19,32 +19,36 @@ class Evaluate:
     self.size_of_stack = size
     self.stack = []
 
+
   def isEmpty(self):
-    """ 
+    """
     Check whether the stack is empty.
     Returns:
       True if it is empty, else returns False.
     """
-    # Write your code here
+      # Write your code here
+
 
   def pop(self):
-    """ 
+    """
     Do pop operation if the stack is not empty.
     Returns:
       The data which is popped out if the stack is not empty.
     """
     # Write your code here
 
+
   def push(self, operand):
-    """ 
+    """
     Push the operand to stack if the stack is not full.
     Arguments:
       operand: The operand to be pushed.
     """
     # Write your code here
 
+
   def validate_postfix_expression(self, expression):
-    """ 
+    """
     Check whether the expression is a valid postfix expression.
     Arguments:
       expression: A String which represents the expression to be validated.
@@ -53,9 +57,10 @@ class Evaluate:
     """
     # Write your code here
 
+
   def evaluate_postfix_expression(self, expression):
-    """ 
-    Evaluate the postfix expression 
+    """
+    Evaluate the postfix expression
     Arguments:
       expression: A String which represents the the expression to be evaluated
     Returns:
@@ -65,10 +70,10 @@ class Evaluate:
 
 
 # Do not change the following code
-postfix_expression = input() # Read postfix expression
+postfix_expression = input()  # Read postfix expression
 tokens = postfix_expression.split()
 evaluate = Evaluate(len(tokens))
 if evaluate.validate_postfix_expression(tokens):
-  print(evaluate.evaluate_postfix_expression(tokens))
+    print(evaluate.evaluate_postfix_expression(tokens))
 else:
-  print('Invalid postfix expression')
+    print('Invalid postfix expression')
